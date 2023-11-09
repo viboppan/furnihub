8dbe0067adea9dabaed2d7fab50f183182f21ab1
Alekhya <alekhyadevisetty97@gmail.com>
2023-11-10 01:41:24 +0530

    Added cart functionalities across pages and updated styles

--- static/cart.js
+++ static/cart.js
@@ -1,55 +1,55 @@
-     function deleteProduct(name){
-        // cartProducts = cartProducts.filter(item => item.name !== name);
-        console.log(name);
+
+let cartProducts = JSON.parse(localStorage.getItem('cart')) || []; // Initialize as an empty array if no data is found
+
+// Use the received data
+document.getElementById('total-items').innerText = cartProducts.length;
+
+const productData = document.getElementById("cart-products");
+let totalCartPrice = 0;
+for (let i = 0; i < cartProducts.length; i++) {
+    totalCartPrice = totalCartPrice + Number(cartProducts[i].cost);
+    const div = document.createElement('div');
+   div.innerHTML = `
+    <div class='d-flex flex-row justify-content-between align-items-center pt-lg-4 pt-2 pb-3 border-bottom mobile'>
+        <div class='d-flex flex-row align-items-center'>
+            <div><img id='image' width='150' height='150' alt='' src=${cartProducts[i].image_url} /></div>
+            <div class='d-flex flex-column pl-md-3 pl-1'>
+                <div><h6>${cartProducts[i].name}</h6></div>
+                <div>Brand: <span class='pl-2'>${cartProducts[i].brand}</span></div>
+                <div>Color:<span class='pl-3'>${cartProducts[i].color}</span></div>
+            </div>
+        </div>
+        <div class='pl-md-0 pl-1'><b>${cartProducts[i].cost}</b></div>
+        <div class='pl-md-0 pl-2'>
+            <span class='fa fa-minus-square text-secondary'></span><span class='px-md-3 px-1'>1</span><span class='fa fa-plus-square text-secondary'></span>
+        </div>
+        <div class='pl-md-0 pl-1'><b>${cartProducts[i].cost}</b></div>
+        <div class='close' id='close-product' onclick='deleteProduct("${cartProducts[i].name}")'>&times;</div>
+    </div>`;
+    productData.appendChild(div);
+}
+document.getElementById('total-cart').innerText = "$" + totalCartPrice + "";
+
+document.getElementById('payment-button').addEventListener('click', function () {
+    window.location.href = `payment_page`;
+});
+
+document.getElementById('go-back').addEventListener('click', function () {
+    window.history.go(-1);
+    return false;
+});
+
+productData.addEventListener('click', function (event) {
+    if (event.target.classList.contains('close')) {
+        const productName = event.target.parentNode.querySelector('h6').innerText;
+        deleteProduct(productName);
     }
+});
 
-function getQueryParam(variable) {
-      const query = window.location.search.substring(1);
-      const vars = query.split("&");
-      for (let i = 0; i < vars.length; i++) {
-        const pair = vars[i].split("=");
-        if (pair[0] === variable) {
-          return decodeURIComponent(pair[1]);
-        }
-      }
-      return null;
-    }
-
-    // Get the data from the URL and parse it as JSON
-    const receivedData = JSON.parse(getQueryParam('cd'));
-    let cartProducts = JSON.parse(JSON.stringify(receivedData));
-
-    // Use the received data
-    console.log("Data received in receiver.html:", cartProducts);
-    document.getElementById('total-items').innerText = cartProducts.length;
-
-    const productData = document.getElementById("cart-products");
-    let totalCartPrice = 0;
-    for (let i = 0; i<cartProducts.length; i++) {
-        totalCartPrice = totalCartPrice+Number(cartProducts[i].cost);
-         const div = document.createElement('div');
-            div.innerHTML = "<div class='d-flex flex-row justify-content-between align-items-center pt-lg-4 pt-2 pb-3 border-bottom mobile'>\n" +
-            "                <div class='d-flex flex-row align-items-center'>\n" +
-            "                    <div><img id='image' width='150' height='150' alt = '' src="+cartProducts[i].image_url+" /></div>\n" +
-            "                    <div class='d-flex flex-column pl-md-3 pl-1'>\n" +
-            "                        <div><h6>"+cartProducts[i].name+"</h6></div>\n" +
-            "                        <div >Brand: <span class='pl-2'>"+cartProducts[i].brand+"</span></div>\n" +
-            "                        <div>Color:<span class='pl-3'>"+cartProducts[i].color+"</span></div>\n" +
-            "                    </div>\n" +
-            "                </div>\n" +
-            "                <div class='pl-md-0 pl-1'><b>"+cartProducts[i].cost+"</b></div>\n" +
-            "                <div class='pl-md-0 pl-2'>\n" +
-            "                    <span class='fa fa-minus-square text-secondary'></span><span class='px-md-3 px-1'>1</span><span class='fa fa-plus-square text-secondary'></span>\n" +
-            "                </div>\n" +
-            "                <div class='pl-md-0 pl-1'><b>"+cartProducts[i].cost+"</b></div>\n" +
-            "                <div class='close' id = 'close-product' onclick='deleteProduct(\"" + cartProducts[i].name + "\")'>&times;</div>\n" +
-            "            </div>"
-        productData.appendChild(div);
-    }
-    document.getElementById('total-cart').innerText = "$"+totalCartPrice+"";
-    document.getElementById('close-product').addEventListener('click',deleteProduct);
-
-
-    document.getElementById('payment-button').addEventListener('click', function() {
-            window.location.href = `payment_page`;
-        });
+// Define the deleteProduct function
+function deleteProduct(name) {
+    cartProducts = cartProducts.filter(item => item.name !== name);
+    console.log(name);
+    localStorage.setItem('cart', JSON.stringify(cartProducts));
+    location.reload(); // Refresh the page after the deletion
+}
--- static/script.js
+++ static/script.js
@@ -3,10 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const addToCartButtons = document.querySelectorAll('.add-to-cart');
     const cartItems = document.getElementById('cart-items');
     const cartTotal = document.getElementById('cart-total');
-
-    let cart = [];
-    let total = 0;
-
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let totalCost = 0;
+    document.getElementById('cart-count').innerText = cart.length;
     addToCartButtons.forEach(button => {
         button.addEventListener('click', function(e) {
 
@@ -19,74 +18,32 @@ document.addEventListener('DOMContentLoaded', function() {
             console.log(product['cost'])
             if (product) {
                 cart.push(product);
-                total += product.cost;
-                updateCart();
+                totalCost += product.cost;
+                document.getElementById('cart-count').innerText = cart.length;
+
+                // updateCart();
             }
-            // receiveDataFromSender(cart);
         });
     });
 
-    function updateCart() {
-        cartItems.innerHTML = '';
-
-        cart.forEach(item => {
-            console.log("from updateCart : "+item)
-            var product = JSON.stringify(item);
-            console.log(product.cost);
-            const listItem = document.createElement('li');
-            listItem.innerText = `${item.name} - ${item.cost}`;
-            cartItems.appendChild(listItem);
-        });
-        cartTotal.textContent = total.toFixed(2);
-    }
+    // function updateCart() {
+    //     cartItems.innerHTML = '';
+    //
+    //     cart.forEach(item => {
+    //         console.log("from updateCart : "+item)
+    //         var product = JSON.stringify(item);
+    //         console.log(product.cost);
+    //         const listItem = document.createElement('li');
+    //         listItem.innerText = `${item.name} - ${item.cost}`;
+    //         cartItems.appendChild(listItem);
+    //     });
+    //     cartTotal.textContent = totalCost.toFixed(2);
+    // }
 
     document.getElementById('go-to-cart-button').addEventListener('click', function() {
             // Redirect to the cart_page.html
-        console.log("cart",JSON.stringify(cart));
-            const encodedData = encodeURIComponent(JSON.stringify(cart));
-            window.location.href = `cart_page?cd=${encodedData}`;
+            let cartString = JSON.stringify(cart);
+            localStorage.setItem('cart', cartString);
+            window.location.href = `cart_page`;
         });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Retrieve cart data from local storage (assuming you're using local storage)
-    const cartData = JSON.parse(localStorage.getItem('cart'));
-
-    const cartItems = document.getElementById('cart-items');
-
-    if (cartData && cartData.length > 0) {
-        cartData.forEach(item => {
-            const cartItemDiv = document.createElement('div');
-            cartItemDiv.classList.add('d-flex', 'flex-row', 'justify-content-between', 'align-items-center', 'pt-4', 'pb-3', 'mobile');
-
-            // Create elements for product details
-            const productImage = document.createElement('img');
-            productImage.src = item.image_url;
-            productImage.width = 150;
-            productImage.height = 150;
-            productImage.alt = 'Product Image';
-
-            const productDetailsDiv = document.createElement('div');
-            const productName = document.createElement('h6');
-            productName.innerText = item.name;
-
-            // Add other product details as needed (Art.No, Color, Size)
-
-            const productPrice = document.createElement('b');
-            productPrice.innerText = `$${item.cost.toFixed(2)}`;
-
-            // Create an element for the quantity controls (e.g., plus/minus buttons)
-
-            // Add a close button for removing items
-
-            // Append all elements to cartItemDiv
-
-            cartItemDiv.appendChild(productImage);
-            cartItemDiv.appendChild(productDetailsDiv);
-            cartItemDiv.appendChild(productPrice);
-            // Append other elements to cartItemDiv
-
-            cartItems.appendChild(cartItemDiv);
-        });
-    }
 });
